8251e565d47293b471d520198f0fcf67fb159ce2
Felipe Parreira <felipeparreirafb@gmail.com>
2018-12-10 14:26:07 -0800

    Refactor state to be self-contained in one object

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
-var board = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0]
-];
-
 // name of the players
 var playerOne = window.prompt('Enter the name of player one (X):', 'First Player');
 var playerTwo = window.prompt('Enter the name of player two (O):', 'Second Player');
 
-// value of each player playing
-var players = [
-  {
-    name: playerOne,
-    value: 1,
-    sign: 'X',
-    score: 0
-  },
+var state = {
 
-  {
-    name: playerTwo,
-    value: 2,
-    sign: 'O',
-    score: 0
-  },
-];
+  board: [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ],
 
-var ties = 0;
-var gameIsFinished = false;
+  players: [
+    {
+      name: playerOne,
+      value: 1,
+      sign: 'X',
+      score: 0
+    },
 
-var renderBoard = function() {
-  console.log('hello')
+    {
+      name: playerTwo,
+      value: 2,
+      sign: 'O',
+      score: 0
+    },
+  ],
+
+  ties: 0,
+
+  gameIsFinished: false,
+
+  turn: 0,
+
+  currentPlayer: 0
+
 };
-
-// state variables
-var turn = 0;
-var currentPlayer = 0;
 
 
 // check if anyone won the game
-var checkIfWon = function(board, row, col) {
-  var rowSum = board[row].reduce((total, el) => total  && players[currentPlayer].value === el, true);
+var checkIfWon = function(row, col) {
+  var rowSum = state.board[row].reduce((total, el) => total  && state.players[state.currentPlayer].value === el, true);
   if (rowSum) {
     return true;
   }
 
-  var colSum = board.reduce((total, row) => total && players[currentPlayer].value === row[col], true);
+  var colSum = state.board.reduce((total, row) => total && state.players[state.currentPlayer].value === row[col], true);
   if (colSum) {
     return true;
   }
     
   if (row + col === 2) {
-    var sumSecDiagonal = board.reduce((total, row, index) => total && players[currentPlayer].value === row[2 - index], true);
+    var sumSecDiagonal = state.board.reduce((total, row, index) => total && state.players[state.currentPlayer].value === row[2 - index], true);
     if (sumSecDiagonal) {
       return true;
     }
   }
 
   if (row === col) {
-    var sumMainDiagonal = board.reduce((total, row, index) => total  && players[currentPlayer].value === row[index], true);
+    var sumMainDiagonal = state.board.reduce((total, row, index) => total  && state.players[state.currentPlayer].value === row[index], true);
     if (sumMainDiagonal) {
       return true;
     }
@@ -68,61 +68,61 @@ var checkIfWon = function(board, row, col) {
 };
 
 var playTurn = function(square) {
-  if (!gameIsFinished) {
-    turn++;
+  if (!state.gameIsFinished) {
+    state.turn++;
     var [row, col] = square.classList[1].split('').map(el => Number(el));
 
-    if (board[row][col] !== 0) {
+    if (state.board[row][col] !== 0) {
       return false;
     }
 
-    var value = players[currentPlayer].value;
+    var value = state.players[state.currentPlayer].value;
 
     // render the board to the UI
-    square.innerText = players[currentPlayer].sign;
+    square.innerText = state.players[state.currentPlayer].sign;
 
-    board[row][col] = value;
+    state.board[row][col] = value;
 
-    if (checkIfWon(board, row, col)) {
-      setTimeout(() => window.alert(`${players[currentPlayer].name} won the game!`), 0);
-      players[currentPlayer].score++;
-      document.getElementsByClassName('score-' + (1 + currentPlayer))[0].innerText = '' + players[currentPlayer].score;
+    if (checkIfWon(row, col)) {
+      setTimeout(() => window.alert(`${state.players[state.currentPlayer].name} won the game!`), 0);
+      state.players[state.currentPlayer].score++;
+      document.getElementsByClassName('score-' + (1 + state.currentPlayer))[0].innerText = '' + state.players[state.currentPlayer].score;
       return true;
-    } else if (turn === 9) {
+    } else if (state.turn === 9) {
       setTimeout(() => window.alert('There was a tie!'), 0);
-      ties++;
-      document.getElementsByClassName('ties-score')[0].innerText = '' + ties;
-      currentPlayer = 0;
+      state.ties++;
+      document.getElementsByClassName('ties-score')[0].innerText = '' + state.ties;
+      state.currentPlayer = 0;
       return true;
     }
 
-    currentPlayer = 1 - currentPlayer;
+    state.currentPlayer = 1 - state.currentPlayer;
     return false;
   }
 };
 
 var resetGame = function() {
   // clean the board
-  for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board.length; j++) {
-      board[i][j] = 0;
+  for (var i = 0; i < state.board.length; i++) {
+    for (var j = 0; j < state.board.length; j++) {
+      state.board[i][j] = 0;
     }
   }
 
   // re-render the board to the UI
-  for (var i = 0; i < board.length; i++) {
-    for(var j = 0; j < board.length; j++) {
+  for (var i = 0; i < state.board.length; i++) {
+    for(var j = 0; j < state.board.length; j++) {
       var squareClass = '' + i + j;
       document.getElementsByClassName(squareClass).valueOf()[0].innerText = '';
     }
   }
 
   // reset state variables
-  turn = 0;
+  state.turn = 0;
 
-  window.alert(`Let's play a new round!\n ${players[currentPlayer].name} starts (since he won last time or was the one to play next)!`);
+  window.alert(`Let's play a new round!\n ${state.players[state.currentPlayer].name} starts (since he won last time or was the one to play next)!`);
 
-  gameIsFinished = false;
+  state.gameIsFinished = false;
 }
 
 window.addEventListener( 'load', function(event) {
@@ -132,14 +132,20 @@ window.addEventListener( 'load', function(event) {
 
   var $squares = Array.from(document.getElementsByClassName('square'));
 
-  $squares.forEach(square => {
-    square.addEventListener('click', (event) => {
-      gameIsFinished = playTurn(event.target) || gameIsFinished;
-    });
-  });
+  // many event Listeners for the board (the foll. commmented lines can be deleted)
+  // $squares.forEach(square => {
+  //   square.addEventListener('click', (event) => {
+  //     gameIsFinished = playTurn(event.target) || gameIsFinished;
+  //   });
+  // });
+
+  var $board = document.getElementsByClassName('board')[0];
+  $board.addEventListener('click', event => {
+    state.gameIsFinished = playTurn(event.target) || state.gameIsFinished;
+  })
 
   var $players = Array.from(document.getElementsByClassName('player'));
-  $players.forEach((player, index) => player.innerText = players[index].name + ` (${players[index].sign})`);
+  $players.forEach((player, index) => player.innerText = state.players[index].name + ` (${state.players[index].sign})`);
   
 });
 
